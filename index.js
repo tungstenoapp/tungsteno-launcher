14f03e1cc467d1214a6768cb31a09e70488c4a31
JoseCarlosGarcia95 <josecarlos.garciaortega@alum.uca.es>
2021-04-18 23:07:10 +0200

    first release!

---
+++ index.js
@@ -0,0 +1,36 @@
+const {
+  app,
+  BrowserWindow
+} = require('electron')
+const path = require('path')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 850,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'webapp/preload.js'),
+      nodeIntegration: true,
+    }
+  })
+
+  win.setMenuBarVisibility(false)
+
+  win.loadFile('webapp/index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
---
+++ webapp/preload.js
@@ -0,0 +1,103 @@
+const {
+    contextBridge,
+    ipcRenderer
+} = require('electron')
+
+const fs = require('fs');
+const path = require('path')
+const download = require('download');
+const request = require("request")
+
+const urlExists = url => new Promise((resolve, reject) => request.head(url).on("response", res => resolve(res.statusCode.toString()[0] === "2")))
+
+var exec = require('child_process').exec;
+const {
+    get
+} = require('http');
+
+function getOS() {
+    var os = process.platform;
+    if (os == "darwin") {
+        os = "mac";
+    } else if (os == "win32" || os == "win64") {
+        os = "win";
+    } else if (os == "linux") {
+        os = "linux";
+    }
+
+    return os;
+}
+
+function getAppDataPath(localFile) {
+    let versionFile = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Preferences' : process.env.HOME + "/.local/share");
+    return versionFile + path.sep + 'tungsteno' + path.sep + localFile;
+}
+
+function getInstalledVersion() {
+    let installedInfo = getAppDataPath('version.json');
+
+    try {
+        if (fs.existsSync(installedInfo)) {
+            let rawdata = fs.readFileSync(installedInfo);
+            return JSON.parse(rawdata);
+        }
+    } catch (err) {}
+    return null;
+}
+
+function launchTungsteno() {
+    let version = getInstalledVersion();
+    let launchFile = getAppDataPath(version['Name']);
+
+    exec(launchFile + " --launcher", function (err, stdout, stderr) {
+        if (err) {
+            throw err;
+        }
+    })
+}
+
+async function waitUntilTungstenoReachable() {
+    let exists = false;
+    try {
+        exists = await urlExists("http://localhost:8000");
+    } catch {}
+
+    while (!exists) {
+        try {
+            exists = await urlExists("http://localhost:8000");
+        } catch {}
+    }
+
+    return Promise.resolve();
+
+}
+
+function initialSetup() {
+    let dir = getAppDataPath('');
+
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+    }
+}
+
+async function downloadTungsteno(latestRelease, basePath) {
+    initialSetup();
+
+    await download(basePath + '/' + latestRelease['Name'], getAppDataPath(''));
+
+    fs.writeFileSync(getAppDataPath('version.json'), JSON.stringify(latestRelease));
+
+    fs.chmodSync(getAppDataPath(latestRelease['Name']), '755');
+
+    return Promise.resolve();
+}
+
+contextBridge.exposeInMainWorld(
+    'electron', {
+        getOS: getOS,
+        getInstalledVersion: getInstalledVersion,
+        downloadTungsteno: downloadTungsteno,
+        launchTungsteno: launchTungsteno,
+        waitUntilTungstenoReachable: waitUntilTungstenoReachable,
+    }
+)
